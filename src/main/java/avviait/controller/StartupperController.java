0a2e5146771aa2c2268a359a92966a50fe61e047
flaprimo <flavio.primo91@gmail.com>
2015-05-21 23:57:03 +0200

    migliorata logout con cancellazione sessione, aggiunta pagina Impostazioni profilo, aggiunto supporto font-awesome, semplificato templating pagine, rimosse dipendenze da model dalla pagina profilo

--- src/main/java/avviait/controller/StartupperController.java
+++ src/main/java/avviait/controller/StartupperController.java
@@ -4,9 +4,11 @@ import avviait.model.Startupper;
 import avviait.model.StartupperFacade;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
+import java.util.Calendar;
 
 @Named
 @SessionScoped
@@ -14,16 +16,32 @@ public class StartupperController implements Serializable {
     @Inject
     private StartupperFacade startupperFacade;
 
+    private Long id;
     private String nome;
     private String cognome;
     private String email;
     private String password;
+    private Calendar dataIscrizione;
+    private String descrizione;
+    private Boolean attivo;
 
     private Startupper startupper;
+
+    private void initStartupper() {
+        this.id = startupper.getId();
+        this.nome = startupper.getNome();
+        this.cognome = startupper.getCognome();
+        this.email = startupper.getEmail();
+        this.password = "";
+        this.dataIscrizione = startupper.getDataIscrizione();
+        this.descrizione = startupper.getDescrizione();
+        this.attivo = startupper.getAttivo();
+    }
 
     public String createStartupper() {
         if (startupperFacade.getStartupperByEmail(email) == null) {
             this.startupper = startupperFacade.createStartupper(nome, cognome, email, password);
+            initStartupper();
             return "createStartupperSuccess";
         } else {
             return "createStartupperFailure";
@@ -34,6 +52,9 @@ public class StartupperController implements Serializable {
         Startupper startupperAttempt = startupperFacade.getStartupperByEmail(email);
         if (startupperAttempt != null && startupperFacade.checkPassword(startupperAttempt, password)) {
             this.startupper = startupperAttempt;
+
+            initStartupper();
+
             return "loginStartupperSuccess";
         } else {
             return "loginStartupperFailure";
@@ -41,8 +62,26 @@ public class StartupperController implements Serializable {
     }
 
     public String logoutStartupper() {
-        startupper = null;
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
         return "logoutStartupperSuccess";
+    }
+
+    public String updateStartupper() {
+        startupper.setEmail(email);
+        if (password != null && !password.equals(""))
+            startupperFacade.changePassword(startupper, password);
+        startupper.setDescrizione(descrizione);
+        startupper.setAttivo(attivo);
+
+        startupperFacade.updateStartupper(startupper);
+        return "updateStartupperSuccess";
+    }
+
+    /**
+     * Getter & Setter
+     * */
+    public Long getId() {
+        return id;
     }
 
     public String getNome() {
@@ -75,6 +114,30 @@ public class StartupperController implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public Calendar getDataIscrizione() {
+        return dataIscrizione;
+    }
+
+    public void setDataIscrizione(Calendar dataIscrizione) {
+        this.dataIscrizione = dataIscrizione;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public Boolean getAttivo() {
+        return attivo;
+    }
+
+    public void setAttivo(Boolean attivo) {
+        this.attivo = attivo;
     }
 
     public Startupper getStartupper() {
